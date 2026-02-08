a83ccbd409ecc90365bf1fbc7575259e4a4ae743
Emir Bolat <emirybolat@hotmail.com>
2026-02-09 00:51:53 +0300

    Update AssemblyInfo with new informational version and refresh cache file

--- obj/Debug/net10.0/TelefonOzellikleri.AssemblyInfo.cs
+++ obj/Debug/net10.0/TelefonOzellikleri.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelefonOzellikleri")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4cc8dd0d357dce55158072c040ab47d405173c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a8253796f6308f6d00312863ce48bd15a3f795")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelefonOzellikleri")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelefonOzellikleri")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
